904f3b39a3bf3d22ce42ffe29652be94752de886
knopperman <knopperman@users.noreply.github.com>
2021-11-04 10:29:24 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="knopperman"
+original_name="lp_endpoints"
+original_urlname="lp-endpoints"
+original_description="Awesome lp_endpoints created by knopperman"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv lp_endpoints $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
